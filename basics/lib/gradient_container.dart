6996cba95192a20769598b7a951afbcec78c1fc7
Gerard Causse <gcausse@epri.com>
2025-04-10 12:22:37 -0400

    My Attempt

--- basics/lib/gradient_container.dart
+++ basics/lib/gradient_container.dart
@@ -5,19 +5,18 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  final List<Color> colors;
+
+  const GradientContainer(this.colors, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: startAlignment,
           end: endAlignment,
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
+          colors: colors,
         ),
       ),
       child: const Center(child: StyledText('Hello, World!')),
--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -3,5 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:basics/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer([
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98),
+        ]),
+      ),
+    ),
+  );
 }
