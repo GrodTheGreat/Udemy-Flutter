9ba783fd2312c9cb766cc7c010f721c73d6ded06
Gerard Causse <grod.causse@gmail.com>
2025-05-22 22:42:37 -0400

    Complete map and spread lesson

--- quiz/lib/questions_screen.dart
+++ quiz/lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,16 +12,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("The question...", style: TextStyle(color: Colors.white)),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
+          ),
           const SizedBox(height: 30),
-          AnswerButton(answerText: "Answer text...", onTap: () {}),
-          AnswerButton(answerText: "Answer text...", onTap: () {}),
-          AnswerButton(answerText: "Answer text...", onTap: () {}),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
         ],
       ),
     );
